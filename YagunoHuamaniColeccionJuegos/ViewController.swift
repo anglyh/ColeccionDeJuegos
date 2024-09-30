382b216126aca5fccbe65208391e3868c50690b1
anglyh <angel.yaguno@tecsup.edu.pe>
2024-09-30 17:01:56 -0500

    Initial Commit

---
+++ YagunoHuamaniColeccionJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YagunoHuamaniColeccionJuegos
+//
+//  Created by Angel Yaguno on 30/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
